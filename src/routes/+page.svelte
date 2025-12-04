ae7efca1fc3f38dffa24cbd5d96742e7a06f1eff
Vincentius Hadianta Wibawa <72688274+errorsolver@users.noreply.github.com>
2025-12-04 23:56:07 +0700

    feat: update home page and add basic structure for login, register, and session routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script></script>
+
+<h1>Home Page PomoChan</h1>
+
+<style></style>
