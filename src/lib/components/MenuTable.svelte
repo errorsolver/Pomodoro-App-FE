fe71d4c9d8d4b5ac8a408966347dc1f09de0fdc0
Vincentius Hadianta Wibawa <72688274+errorsolver@users.noreply.github.com>
2025-12-09 01:08:48 +0700

    Add new SVG icons and MenuTable component
    - Added BtnPlay.svg and BtnPrev.svg icons for play and previous actions.
    - Introduced DotActive.svg and DotInActive.svg icons for active and inactive states.
    - Created MenuTable.svelte component for a sticky menu with buttons for stopwatch, calendar, task list, settings, and logout.
    - Updated calendar.svg with minor path adjustments for improved rendering.

---
+++ src/lib/components/MenuTable.svelte
@@ -0,0 +1,22 @@
+<div class="flex flex-col items-center mt-8 sticky bottom-0">
+  <div class="mt-10">
+    <img src="/icons/meja.svg" alt="desk menu" />
+  </div>
+  <div class="absolute grid grid-cols-5 gap-12">
+    <button on:click={alert('stopwatch')}>
+      <img src="/icons/stopwatch.svg" alt="stopwatch" />
+    </button>
+    <button on:click={alert('calendar')}>
+      <img src="/icons/calendar.svg" alt="calendar" />
+    </button>
+    <button on:click={alert('task list')}>
+      <img src="/icons/task-list.svg" alt="task list" />
+    </button>
+    <button on:click={alert('settings menu')}>
+      <img src="/icons/settings.svg" alt="settings menu" />
+    </button>
+    <button on:click={alert('logout')}>
+      <img src="/icons/logout.svg" alt="logout" />
+    </button>
+  </div>
+</div>
