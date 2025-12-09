7af3186f95d371b7d8137c3ecc6ef81e50096c09
Vincentius Hadianta Wibawa <72688274+errorsolver@users.noreply.github.com>
2025-12-10 01:01:26 +0700

    feat: enhance registration page with new design and password visibility toggle
    - Updated the registration page layout to include a new header and description.
    - Added input fields for full name, email, and password with a toggle for password visibility.
    - Included SVG images for visual enhancement.
    - Styled input fields for better user experience.
    - Added a link to the login page for users who already have an account.
    - Introduced a new font class for regular text styling.

--- src/routes/(auth)/register/+page.svelte
+++ src/routes/(auth)/register/+page.svelte
@@ -1,1 +1,98 @@
-<h1>Register</h1>
+<script>
+    import BtnCreateASpace from '$lib/assets/img/BtnCreateASpace.svg?url';
+    import SleepingWoman from '$lib/assets/img/SleepingWoman.svg?url';
+    import EyeOpen from '$lib/assets/img/EyeOpen.svg?url';
+    import EyeClosed from '$lib/assets/img/EyeClosed.svg?url';
+
+    let isShowPassword = false;
+</script>
+
+<div class="flex gap-40 justify-center h-screen items-center">
+    <div>
+        <img src={SleepingWoman} alt="Sleeping Woman" />
+    </div>
+
+    <div>
+        <h1 class="text-5xl font-bold mb-2">Create a space</h1>
+        <p class="patrickhand mb-8">A calm place to keep your focus.</p>
+
+        <form class="flex flex-col">
+            <label for="spaceName" class="font-bold">Full Name</label>
+            <input
+                id="spaceName"
+                class="mb-4"
+                name="spaceName"
+                type="text"
+                placeholder="Enter your space name"
+            />
+
+            <label for="email" class="font-bold">Email</label>
+            <input
+                id="email"
+                class="mb-4"
+                name="email"
+                type="email"
+                placeholder="Enter your email"
+            />
+
+            <label for="password" class="font-bold">Password</label>
+            <div class="input-wrapper mb-4">
+                <input
+                    id="password"
+                    class="w-full"
+                    name="password"
+                    type={isShowPassword ? 'text' : 'password'}
+                    placeholder="Enter your password"
+                />
+                <span class="eye">
+                    <button
+                        class="block"
+                        on:click={() => (isShowPassword = !isShowPassword)}
+                        style="cursor: pointer;"
+                    >
+                        {#if isShowPassword}
+                            <img
+                                src={EyeOpen}
+                                class="-translate-y-1"
+                                alt="Eye Closed"
+                            />
+                        {:else}
+                            <img src={EyeClosed} alt="Eye Open" />
+                        {/if}
+                    </button>
+                </span>
+            </div>
+
+            <button type="submit" class="mt-4 cursor-pointer">
+                <img src={BtnCreateASpace} alt="" />
+            </button>
+        </form>
+        <p class="mt-4 text-center patrickhand">
+            Already have an account?
+            <a href="/login" class="caveatregular underline font-bold"
+                >Sign In</a
+            >
+        </p>
+    </div>
+</div>
+
+<style>
+    input {
+        background-color: transparent;
+        box-shadow: none;
+    }
+
+    input:focus {
+        border-color: unset;
+        box-shadow: unset;
+    }
+
+    .input-wrapper {
+        position: relative;
+    }
+    .input-wrapper .eye {
+        position: absolute;
+        top: 50%;
+        right: 15px;
+    }
+</style>
