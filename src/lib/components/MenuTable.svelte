9e06594c1039b8a83fcf2052ac14148eafe8d4f4
Vincentius Hadianta Wibawa <72688274+errorsolver@users.noreply.github.com>
2025-12-09 15:33:06 +0700

    fix(MenuTable): replace buttons with links for better navigation and accessibility fix(layout): prevent user selection and dragging for improved UI interaction

--- src/lib/components/MenuTable.svelte
+++ src/lib/components/MenuTable.svelte
@@ -1,22 +1,24 @@
-<div class="flex flex-col items-center justify-self-center fixed bottom-0 w-screen z-999 bg-linear-to-t from-white from-41% to-transparent to-88%">
+<div
+  class="flex flex-col items-center justify-self-center fixed bottom-0 pb-4 w-screen z-999 bg-linear-to-t from-white from-54% to-transparent to-88%"
+>
   <div class="mt-10">
     <img src="/icons/meja.svg" alt="desk menu" />
   </div>
   <div class="absolute grid grid-cols-5 gap-12">
-    <button on:click={alert('stopwatch')}>
+    <a href="/home-screen">
       <img src="/icons/stopwatch.svg" alt="stopwatch" />
-    </button>
-    <button on:click={alert('calendar')}>
+    </a>
+    <a href={void 0} class="cursor-not-allowed">
       <img src="/icons/calendar.svg" alt="calendar" />
-    </button>
-    <button on:click={alert('task list')}>
+    </a>
+    <a href="/session/incomplete">
       <img src="/icons/task-list.svg" alt="task list" />
-    </button>
-    <button on:click={alert('settings menu')}>
+    </a>
+    <a href={void 0} class="cursor-not-allowed">
       <img src="/icons/settings.svg" alt="settings menu" />
-    </button>
-    <button on:click={alert('logout')}>
+    </a>
+    <a href={void 0} class="cursor-not-allowed">
       <img src="/icons/logout.svg" alt="logout" />
-    </button>
+    </a>
   </div>
 </div>
