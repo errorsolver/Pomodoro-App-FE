456d2486a49edc6b011196c88940e4fa0a1bc206
Vincentius Hadianta Wibawa <72688274+errorsolver@users.noreply.github.com>
2025-12-11 13:19:16 +0700

    fix: add color styles for improved text visibility across elements

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
 	import './layout.css';
 	import pomoChanLogo from '$lib/assets/logo.svg?url';
-	
+
 	let { children } = $props();
 </script>
 
 <svelte:head>
+	<title>Pomo Chan</title>
+	<meta name="description" content="Pomo Chan adalah aplikasi Pomodoro dengan karakter cewek Jepang yang manis dan penuh semangat. Ia bakal nemenin kamu fokus, kasih dukungan imut, dan bikin kerja atau belajar jadi lebih fun serta bikin hati hangat setiap sesi." />
 	<link rel="icon" href={pomoChanLogo} />
 </svelte:head>
 
