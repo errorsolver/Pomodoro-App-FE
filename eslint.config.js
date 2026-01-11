0edbdc48478660ef5a1eeaf88665744def476487
Vincentius Hadianta Wibawa <72688274+errorsolver@users.noreply.github.com>
2026-01-12 02:56:27 +0700

    fix: add process as readonly global in ESLint configuration for improved type safety

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,7 @@ import svelteParser from "svelte-eslint-parser";
 
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: { ...globals.browser, process: 'readonly' } } },
   { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
   { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
   { files: ["**/*.json5"], plugins: { json }, language: "json/json5", extends: ["json/recommended"] },
